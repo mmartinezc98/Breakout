fea9a9ed363da6bf6a82a548b621cfdec66a67b8
Mario Clase <mariomartinezcarrillo98@gmail.com>
2025-10-28 21:09:42 +0100

    Creacion de Movimiento de Pala

--- Assets/Scripts/Pala/Movimiento.cs
+++ Assets/Scripts/Pala/Movimiento.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
+    private Transform _transform;
+    public float Velocity = 2f;
+
+    private void Awake()
+    {
+        this._transform=this.gameObject.GetComponent<Transform>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,24 @@ public class Movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveObject();
+    }
+
+
+
+    private void MoveObject()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        Vector3 movementVector = new Vector3();
+
+        movementVector.x= horizontalMove;
+        movementVector.y = 0;
+        movementVector.z= 0;
+
+
+        this._transform.Translate(movementVector.normalized * Velocity * Time.deltaTime);
+
+
+
     }
 }
