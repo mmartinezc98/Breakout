57bf59b91bebc901a2dd72b3e374941214f2520e
mmartinezc98 <mariomartinezcarrillo98@gmail.com>
2025-11-25 18:24:26 +0100

    TO_DO: continuar pantalla de victoria. Arreglar problema al perder partida en el nivel 1 y volver a reiniciarla desde el menu principal.

---
+++ Assets/Scripts/WINNER/WinnerManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinnerManager : MonoBehaviour
+{
+    public GameObject WinnerButton;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReturnMainMenu();
+    }
+
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene("Pantalla Inicio");
+    }
+}
